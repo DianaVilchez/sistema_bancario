49f897aa75cd9492b5ab5cf9adb77a905f13b262
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-10-10 10:57:14 -0600

    cambios .env, node_modules, carpeta router, controller y package.json

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE || !process.env.DB_PASSWORD || !process.env.DB_HOST) {
     throw new Error('Las variables de entorno para la base de datos no están definidas');
 }
 // Configura la conexión a la base de datos
-export const sequelize = new Sequelize(process.env.DATABASE,process.env.USER ,process.env.PASSWORD, {
+export const sequelize = new Sequelize(process.env.DATABASE,process.env.USER ,process.env.DB_PASSWORD, {
     host: 'localhost', 
     dialect: 'postgres', 
     logging: false
